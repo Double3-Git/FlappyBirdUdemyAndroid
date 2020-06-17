89a81e52e94e19a245b138b99e41873ace1bb219
Double 3 <double3.2017@gmail.com>
2020-06-17 21:02:19 +0300

    Movig ground

---
+++ Assets/Scripts/RepositionGround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepositionGround : MonoBehaviour
+{
+
+    BoxCollider2D boxCollider2D;
+    float totalLenght;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        totalLenght = boxCollider2D.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= -totalLenght)
+        {
+            transform.position = (Vector2)transform.position + Vector2.right * totalLenght * 2f;
+        }
+    }
+}
