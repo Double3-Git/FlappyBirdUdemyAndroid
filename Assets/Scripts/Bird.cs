733ac191831c851db6afdbdfbdf5389321533cee
Double 3 <double3.2017@gmail.com>
2020-06-17 07:33:23 +0300

    Bird flap movement

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Bird : MonoBehaviour
 {
+    [Header("Player settings")]
+    public float speed = 3f;
+
+    // Private section
+    Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb2d.velocity = Vector2.up * speed;
+        }
     }
 }
