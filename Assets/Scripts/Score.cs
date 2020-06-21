a9d1a8cef439cd8ad40dfc9f01b74893c072e2e4
Double 3 <double3.2017@gmail.com>
2020-06-21 11:29:18 +0300

    Public score to access it

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    static int score;
+    public static int score;
     // Start is called before the first frame update
     void Start()
     {
