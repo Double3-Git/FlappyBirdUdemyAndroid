9fb930fde8bdb9966c651a832917ef71d7be1a80
Double 3 <double3.2017@gmail.com>
2020-06-21 12:03:41 +0300

    GameManager initial setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("UI Settings")]
+    public GameObject scorePanel;
+    public GameObject gameOverPanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        scorePanel.SetActive(false);
+        gameOverPanel.SetActive(true);
+    }
+
+}
