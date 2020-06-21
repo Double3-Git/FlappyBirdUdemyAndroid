94088c857487e4bdd5dca809f07d9917810d76ec
Double 3 <double3.2017@gmail.com>
2020-06-21 20:45:37 +0300

    HighScore implementation

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,16 +5,30 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    [Header("UI Settings")]
+    public Text panelScoreText;
+    public Text panelHighScoreText;
+
     public static int score;
+    int highScore;
+
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+        highScore = PlayerPrefs.GetInt("highScore");
     }
 
     // Update is called once per frame
     void Update()
     {
         GetComponent<Text>().text = score.ToString();
+        panelScoreText.text = score.ToString();
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("highScore", highScore);
+            panelScoreText.text = highScore.ToString();
+        }
     }
 }
